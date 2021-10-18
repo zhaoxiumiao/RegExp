83533a4ca91d52a3fadf81274edcd04fcc2f5067
DESKTOP-3IDRGCI\fool <1027583934@qq.com>
2021-10-18 19:59:27 +0800

    "1.2.3."

---
+++ chapter1/index.js
@@ -0,0 +1,13 @@
+// 横向模糊匹配
+
+// 长度是不固定的，可以是多种情况
+// const regex = /ab{2,5}c/g;
+// const string = "abc abbc abbbc abbbbc abbbbbc abbbbbbc";
+// console.log(string.match(regex));
+
+// 纵向模糊匹配
+
+// 不是某个确定的字符，可以有多种可能
+const regex = /a[123]b/g
+const string = "a0b a1b a2b a3b a4b"
+console.log(string.match(regex));
