a98e1e4ac5701043ce7976b8e048e2e97ec969ac
DESKTOP-3IDRGCI\fool <1027583934@qq.com>
2021-10-20 09:24:28 +0800

    "贪婪匹配"

--- chapter1/index.js
+++ chapter1/index.js
@@ -8,6 +8,18 @@
 // 纵向模糊匹配
 
 // 不是某个确定的字符，可以有多种可能
-const regex = /a[123]b/g
-const string = "a0b a1b a2b a3b a4b"
+// const regex = /a[123]b/g
+// const string = "a0b a1b a2b a3b a4b"
+// console.log(string.match(regex));
+
+// 贪婪匹配
+
+// const regex = /\d{2,5}/g
+// const string = "123 1234 12345 123456"
+// console.log(string.match(regex));
+
+// 惰性匹配
+
+const regex = /\d{2,5}?/g
+const string = "123 1234 12345 123456"
 console.log(string.match(regex));
